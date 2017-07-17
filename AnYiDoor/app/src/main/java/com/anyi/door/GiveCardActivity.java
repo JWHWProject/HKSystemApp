6a1b2178dc95838f22157b90175cb9b1f9ecbcef
jiawei <543126764@qq.com>
2017-07-17 11:16:10 +0800

    上传 NFC卡 刷卡代码

--- AnYiDoor/app/src/main/java/com/anyi/door/GiveCardActivity.java
+++ AnYiDoor/app/src/main/java/com/anyi/door/GiveCardActivity.java
@@ -1,11 +1,21 @@
 package com.anyi.door;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +34,8 @@ import android.widget.TextView;
 import com.anyi.door.utils.Config;
 import com.anyi.door.utils.DbTOPxUtils;
 import com.anyi.door.utils.MyGridView;
+import com.anyi.door.utils.card.CardManager;
+import com.anyi.door.utils.card.Util;
 import com.zzti.fengyongge.imagepicker.PhotoPreviewActivity;
 import com.zzti.fengyongge.imagepicker.PhotoSelectorActivity;
 import com.zzti.fengyongge.imagepicker.model.PhotoModel;
@@ -56,6 +68,8 @@ import cn.nj.www.my_module.tools.ImageLoaderUtil;
 import cn.nj.www.my_module.tools.NetLoadingDialog;
 import cn.nj.www.my_module.tools.ToastUtil;
 
+import static com.anyi.door.R.id.et_card_number;
+
 
 /**
  * 发卡
@@ -69,7 +83,7 @@ public class GiveCardActivity extends BaseActivity implements View.OnClickListen
     @Bind(R.id.tv_sex)
     TextView tvSex;
 
-    @Bind(R.id.et_card_number)
+    @Bind(et_card_number)
     EditText etCardNumber;
 
     @Bind(R.id.et_name)
@@ -119,14 +133,95 @@ public class GiveCardActivity extends BaseActivity implements View.OnClickListen
 
     private int maxSize = 4;
 
+
+    private NfcAdapter nfcAdapter;
+    private PendingIntent pendingIntent;
+    private IntentFilter[] mFilters;
+    private String[][] mTechLists;
+    private boolean isFirst = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_give_card);
         ButterKnife.bind(this);
         initAll();
+        // 获取nfc适配器，判断设备是否支持NFC功能
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            return;
+        } else if (!nfcAdapter.isEnabled()) {
+            return;
+        }
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
+                getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        ndef.addCategory("*/*");
+        mFilters = new IntentFilter[]{ndef};// 过滤器
+        mTechLists = new String[][]{
+                new String[]{MifareClassic.class.getName()},
+                new String[]{NfcA.class.getName()}};// 允许扫描的标签类型
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, mFilters,
+                mTechLists);
+        if (isFirst) {
+            if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(getIntent().getAction())) {
+                String result = processIntent(getIntent());
+                String str=Util.hex2Decimal(result);
+                etCardNumber.setText(str+"");
+            }
+            isFirst = false;
+        }
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // TODO Auto-generated method stub
+        super.onNewIntent(intent);
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+            String result = processIntent(intent);
+            String str=Util.hex2Decimal(result);
+            etCardNumber.setText(str+"");
+        }
+    }
+    /**
+     * 获取tab标签中的内容
+     *
+     * @param intent
+     * @return
+     */
+    @SuppressLint("NewApi")
+    private String processIntent(Intent intent) {
+        Intent intent1=intent;
+        String intentActionStr=intent1.getAction();
+        String strId="";
+        Log.e("sub","intentActionStr="+intentActionStr);
+//        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intentActionStr)
+//                ||NfcAdapter.ACTION_TECH_DISCOVERED.equals(intentActionStr)
+//                ||NfcAdapter.ACTION_TAG_DISCOVERED.equals(intentActionStr)) {
+        byte[] bytesId = intent1.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+//            Tag tag = intent1.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+//            byte[] bytesId = tag.getId();
+        strId= Util.toHexString(bytesId,0,bytesId.length);
+//        }
+        if(strId!=null&&!strId.equals("")){
+            return strId;
+        }
+        Parcelable[] rawmsgs = intent
+                .getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (rawmsgs != null && rawmsgs.length > 0) {
+            NdefMessage msg = (NdefMessage) rawmsgs[0];
+            NdefRecord[] records = msg.getRecords();
+            String resultStr = new String(records[0].getPayload());
+            return resultStr;
+        } else {
+            final Parcelable p = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String str = (p != null) ? CardManager.load(p, getResources()) : null;
+            return str;
+        }
+    }
 
     private void initTitle() {
         View view = findViewById(R.id.common_back);
