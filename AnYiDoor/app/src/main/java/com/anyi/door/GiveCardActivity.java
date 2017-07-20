d31e00b942d91d8b5e1ad7582cfe922b85a4f2be
jiawei <543126764@qq.com>
2017-07-20 19:15:33 +0800

    上传修改的问题

--- AnYiDoor/app/src/main/java/com/anyi/door/GiveCardActivity.java
+++ AnYiDoor/app/src/main/java/com/anyi/door/GiveCardActivity.java
@@ -218,6 +218,7 @@ public class GiveCardActivity extends BaseActivity implements View.OnClickListen
                 String result = processIntent(getIntent());
                 String str = Util.hex2Decimal(result);
                 etCardNumber.setText(str + "");
+                etName.requestFocus();
             }
             isFirst = false;
         }
@@ -233,6 +234,7 @@ public class GiveCardActivity extends BaseActivity implements View.OnClickListen
             String result = processIntent(intent);
             String str = Util.hex2Decimal(result);
             etCardNumber.setText(str + "");
+            etName.requestFocus();
         }
     }
 
@@ -488,10 +490,12 @@ public class GiveCardActivity extends BaseActivity implements View.OnClickListen
                                                         case 0:
                                                             llInner.setVisibility(View.GONE);
                                                             llOuter.setVisibility(View.VISIBLE);
+                                                            etName.setHint("请输入外来人员姓名");
                                                             break;
                                                         case 1:
                                                             llInner.setVisibility(View.VISIBLE);
                                                             llOuter.setVisibility(View.GONE);
+                                                            etName.setHint("请输入企业人员姓名");
                                                             break;
                                                     }
                                                 }
